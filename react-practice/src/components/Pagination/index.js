4f291d2effa16fe88218459b5804fda1b1142bdf
劉芊秀 <shirley@pchome.tw>
2022-07-04 17:15:43 +0800

    react練習-資料用陣列帶入

--- react-practice/src/components/Pagination/index.js
+++ react-practice/src/components/Pagination/index.js
@@ -3,13 +3,23 @@ import "./pagination.scss";
 
 
 const Pagination = () => {
-
+	const total = 3;
 	const [page, setPage] = useState(1);
+	const subtract = () => {
+		if(page > 1){
+			setPage(page - 1);
+		}
+	};
+	const add = () => {
+		if (page < total){
+			setPage(page + 1);
+		}
+	};
 	return (
 		<div className="c-pagination">
-			<div className="c-pagination__button c-pagination__button--prev"></div>
-			<div className="c-pagination__value"><span>{page}</span>/<span>3</span></div>
-			<div className="c-pagination__button c-pagination__button--next"></div>
+			<div className="c-pagination__button c-pagination__button--prev" onClick={subtract}></div>
+			<div className="c-pagination__value"><span>{page}</span>/<span>{total}</span></div>
+			<div className="c-pagination__button c-pagination__button--next" onClick={add}></div>
 		</div>
 	);
 };
--- react-practice/src/components/Theme/index.js
+++ react-practice/src/components/Theme/index.js
@@ -2,25 +2,36 @@ import React from "react";
 import "./theme.scss"
 import Banner from "../Banner";
 
-const Tag = (props) => {
-	const { text } = props;
-	return (
-		<a href="https://24h.pchome.com.tw/" className="c-tag">{text}</a>
-	);
-};
+// const Tag = (props) => {
+// 	const { text } = props;
+// 	return (
+// 		<a href="https://24h.pchome.com.tw/" className="c-tag">{text}</a>
+// 	);
+// };
 
 const Theme = () => {
+	const tag = [
+		{id:1, title: '曜越電競椅'}, 
+		{id:2, title: '羅技滑鼠下殺9折'}, 
+		{id:3, title: 'HP電競筆電'}, 
+		{id:4, title: 'Acer主機+電競耳麥優惠組'}, 
+		{id:5, title: '2022喇叭推薦指南'}
+	];
+	const tagItems = tag.map((item) =>
+		<li key={item.id}><a href="#" className="c-tag">{item.title}</a></li>
+	);
 	return (
 		<div className="c-theme">
 			<div className="c-theme__content">
 				<div className="c-theme__tag">主題推薦</div>
 				<h3 className="c-theme__title">3C 電競年中戰</h3>
 				<ul className="c-theme__keyWordTags">
-					<li><Tag text="曜越電競椅" /></li>
+					{tagItems}
+					{/* <li><Tag text="曜越電競椅" /></li>
 					<li><Tag text="羅技滑鼠下殺9折" /></li>
 					<li><Tag text="HP電競筆電" /></li>
 					<li><Tag text="Acer主機+電競耳麥優惠組" /></li>
-					<li><Tag text="2022喇叭推薦指南" /></li>
+					<li><Tag text="2022喇叭推薦指南" /></li> */}
 				</ul>
 			</div>
 			<Banner />
