37e1b7a88c114e570b8bcb4331f8555f957dad0d
劉芊秀 <shirley@pchome.tw>
2022-07-05 11:25:57 +0800

    react練習-調整 data和 Items元件化

--- react-practice/src/components/ProdList/index.js
+++ react-practice/src/components/ProdList/index.js
@@ -1,45 +1,30 @@
 import React from "react";
-import "./prodList.scss"
-import Pagination from "../Pagination"
-
-// const Prod = (props) => {
-// 	const { imgSrc, name, price } = props;
-// 	return (
-// 		<div className="c-prod">
-// 			<img className="c-prod__img" src={imgSrc} alt={name} />
-// 			<h3 className="c-prod__name">{name}</h3>
-// 			<div className="c-prod__price">${price}</div>
-// 		</div>
-// 	);
-// };
+import "./prodList.scss";
+import Pagination from "../Pagination";
+import { data } from '../../data';
 
 const ProdList = () => {
-	const content = [
-		{id:1, image:'https://fakeimg.pl/320x320/?text=1st',
-		name:'test1',
-		price:1111},
-		{id:2, image:'https://fakeimg.pl/320x320/',name:'test2222222',price:222},
-		{id:3, image:'https://fakeimg.pl/320x320/?text=3rd',name:'test333',price:33},
-		{id:4, image:'https://fakeimg.pl/320x320/?text=4th',name:'test444',price:4444},
-		{id:5, image:'https://cs-f.ecimg.tw/items/DHAS4MA900DV9SO/000002_1643264511.jpg',
-		name:'G733ZM-0022S12900H 電競筆電',
-		price:56900},
-		{id:6, image:'https://fakeimg.pl/320x320/?text=6th',name:'test6',price:666},
-	];
-	const listItems = content.map((item) =>
-		<li key={item.id}>
-			<a className="c-prod" href="#">
-				<img className="c-prod__img" src={item.image} alt={item.name} />
-				<h3 className="c-prod__name">{item.name}</h3>
-				<div className="c-prod__price">${item.price}</div>
-			</a>
-		</li>
-	);
+	
+	const ListItems =()=>{
+		return data.map((item) =>
+		{
+			console.log(item);
+
+			return (
+			<li key={item.id}>
+				<a className="c-prod" href="#">
+					<img className="c-prod__img" src={item.image} alt={item.name} />
+					<h3 className="c-prod__name">{item.name}</h3>
+					<div className="c-prod__price">${item.price}</div>
+				</a>
+			</li>
+			)
+		})
+	}
 	return (
 		<div className="c-prodList">
 			<ul className="c-prodList__list">
-				{/* <li><Prod imgSrc="https://cs-f.ecimg.tw/items/DHAS4MA900DV9SO/000002_1643264511.jpg" name="G733ZM-0022S12900H 電競筆電" price="56900" /></li> */}
-				{listItems}
+				{ListItems()}
 			</ul>
 			<Pagination />
 		</div>
---
+++ react-practice/src/data.js
@@ -0,0 +1,23 @@
+const data = [
+	{id:1, image:'https://fakeimg.pl/320x320/?text=1st',
+	name:'test1',
+	price:1111},
+	{id:2, image:'https://fakeimg.pl/320x320/',name:'test2222222',price:222},
+	{id:3, image:'https://fakeimg.pl/320x320/?text=3rd',name:'test333',price:33},
+	{id:4, image:'https://fakeimg.pl/320x320/?text=4th',name:'test444',price:4444},
+	{id:5, image:'https://cs-f.ecimg.tw/items/DHAS4MA900DV9SO/000002_1643264511.jpg',
+	name:'G733ZM-0022S12900H 電競筆電',
+	price:56900},
+	{id:6, image:'https://fakeimg.pl/320x320/?text=6th',name:'test6',price:666},
+];
+
+const tag = [
+	{id:1, title: '曜越電競椅'}, 
+	{id:2, title: '羅技滑鼠下殺9折'}, 
+	{id:3, title: 'HP電競筆電'}, 
+	{id:4, title: 'Acer主機+電競耳麥優惠組'}, 
+	{id:5, title: '2022喇叭推薦指南'}
+];
+
+
+export { data, tag };
