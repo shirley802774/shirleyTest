3c2e867c0ba96c0a79f959a61c83429fef8f54e3
劉芊秀 <shirley@pchome.tw>
2022-09-02 17:08:28 +0800

    feature/react練習:嘗試切換

--- react-practice/src/components/ToolBar/index.js
+++ react-practice/src/components/ToolBar/index.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 
-const ToolBar = ({dataTheme, tabData}) => {
-	const changeTheme = () =>{
-		
+const ToolBar = ({dataTheme, setDataTheme, setDataProd, tabData}) => {
+	const [prodDataId, setProdDataId] = useState(dataTheme.Id);
+	const changeTheme = (prodData, prodId) =>{
+		setDataProd(prodData.Nodes);
+		setProdDataId(prodId);
 	}
 
 	return (
@@ -14,7 +16,7 @@ const ToolBar = ({dataTheme, tabData}) => {
 			<ul className="c-toolBar__content">
 				{
 					tabData.map((item) =>
-					(<li key={item.BlockId} className={`c-toolBar__item ${item.BlockId === dataTheme?.Id ? "is_active" : ""}`} onClick={changeTheme}>{item.Nodes[0].Link.Text}</li>)
+					(<li key={item.BlockId} className={`c-toolBar__item ${item.BlockId === prodDataId ? "is_active" : ""}`} onClick={changeTheme(item, item.BlockId)}>{item.Nodes[0].Link.Text}</li>)
 					)
 				}
 				{/* <li className={"c-toolBar__item " + active} onClick={isActive(1)}>{dataTheme?.Link?.Text}</li> */}
